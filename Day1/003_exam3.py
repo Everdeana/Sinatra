fb4cf4f8385045fe4a73a2e0bd13f13605181e81
Everdeana <87472190+Everdeana@users.noreply.github.com>
2024-05-01 16:37:29 +0900

    Update 003_exam3.py

--- Day1/003_exam3.py
+++ Day1/003_exam3.py
@@ -1,1 +1,64 @@
-ㅇㅇ
+'''
+# 작성일 : 2024년 5월 1일
+# 파일명 : 함수
+# 개발자 : 장국진
+# 사용 라이브러리 : 
+'''
+
+def AddFunc(x, y):
+	z0 = x + y
+	z1 = x - y
+	z2 = x * y
+	z3 = x // y
+	z4 = x % y
+	return z0, z1, z2, z3, z4 # 여러 변수들을 받을 수 있음
+
+# a0, a1, a2, a3, a4 = AddFunc(5, 4)
+
+# print(a0)
+# print(a1)
+# print(a2)
+# print(a3)
+# print(a4)
+
+print("-"*60)
+
+a0, _, a2, _, a4 = AddFunc(5, 4) # 데이터는 받았지만 사용 X
+
+print(a0)
+# print(a1)
+print(a2)
+# print(a3)
+print(a4)
+
+print("-"*60)
+
+def AddFunc1(x, y = 3): #  default값 설정
+	b1 = x + y
+	return b1
+
+print(AddFunc1(5)) # default값 있으므로 y 입력 없으면 y에 3 저장
+
+print("-"*60)
+
+#########################################################################################################
+# 자료형 : 배열(list) --> 추가/삭제 가능, 튜플(tuple) --> 추가/삭제 불가능, 딕셔너리(dictionary)
+# 배열 [], 튜플 (), 딕셔너리 {}
+# 딕셔너리 구조 --> json과 유사한 형태 --> 데이터 송수신시 json 사용하므로 python에선 딕셔너리 많이 사용
+# 배열 + 딕셔너리 = 데이터 전송
+
+arrList = [[10, 20, 30], [20, 21, 22], [30, 31, 32]] # 인공지능에선 2차원배열(다중배열) 사용
+print(arrList)
+print(type(arrList))
+
+# 앞에서 ->  0 	 1   2	 3	 4
+arrList1 = [10, 20, 30, 40, 50]
+# 뒤에서 ->	-5	-4	-3	-2	-1
+print(arrList1[0])
+print(arrList1[-1])
+
+print("-"*60)
+
+# 슬라이싱(배열, PANDAS)
+arrList1 = [1, 2, 3, 4, 5] # 3, 4, 5만 슬라이싱
+sliceList1 = arrList1[]
