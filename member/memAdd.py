80fc052cfd20eb973120736195760f577908d778
Everdeana <87472190+Everdeana@users.noreply.github.com>
2024-05-14 16:58:46 +0900

    Add files via upload

--- member/memAdd.py
+++ member/memAdd.py
@@ -17,24 +17,37 @@ class MemAddWin(QDialog, memAddMain): # 윈도우, ui파일 둘 다 가져옴 (D
 	
 
 	def btn_add_clicked(self):
-		if (self.le_mail.text() == '' or 
-	  		self.le_name.text() == '' or 
-			self.le_telno.text() == '' or
-			self.le_addr.text() == '' ):
-			QMessageBox.about(self, "에러", "필수 입력사항을 입력해주세요!!!.")
+		if self.le_mail.text() == '':
+			QMessageBox.about(self, "입력에러", "메일을 입력해주세요!!!.")
+			return
+		if self.le_name.text() == '':
+			QMessageBox.about(self, "입력에러", "이름을 입력해주세요!!!.")
+			return
+		if self.le_telno.text() == '':
+			QMessageBox.about(self, "입력에러", "전화번호를 입력해주세요!!!.")
+			return
+		if self.le_addr.text() == '':
+			QMessageBox.about(self, "입력에러", "주소를 입력해주세요!!!.")
 			return
 		# 간결함을 위해 else를 쓰지 않고 return 후 바로 나감 --> 한 루틴에선 한 가지만 수행
-		
-		MemDbAdd(self.le_mail.text(), self.le_name.text(), self.le_telno.text(), self.le_addr.text(), self.le_sns.text())
-		'''
+		try:
+			# 변수가 필요하지 않을 땐 변수 사용 X
+			MemDbAdd(self.le_mail.text(), 
+				self.le_name.text(), 
+				self.le_telno.text(), 
+				self.le_addr.text(), 
+				self.le_sns.text()
+			)
+			'''
 		print(self.memMail)
 		print(self.memName)
 		print(self.memTelno)
 		print(self.memAddr)
 		print(self.memSns)
 		'''
-		
-
+			QMessageBox.about(self, "입력", "데이터베이스에 기록되었습니다.")
+		except:
+			QMessageBox.about(self, "입력에러", "데이터베이스 입력 실패하였습니다!!!.")
 	def btn_close_clicked(self):
 		self.close() # 자기 자신만 종료
 	
--- member/memdbEdit.py
+++ member/memdbEdit.py
@@ -7,13 +7,13 @@ import requests
 # DB 저장 함수
 #########################################################################################################
 
-def MemDbAdd(mail, name, telno, addr, sns):
+def MemDbEdit(idx, mail, name, telno, addr, sns):
 
 	# DB Setting
 	conn = pymysql.connect(
 		host 	 = '127.0.0.1', # == host = 'localhost' --> 자기 자신 의미
 		user 	 = 'mem',
-		password ='@ai1234@',
+		password = '@ai1234@',
 		port 	 = 3306,
 		db 		 = 'memdb',
 		charset  = 'utf8' # 기본 변수들
@@ -23,20 +23,28 @@ def MemDbAdd(mail, name, telno, addr, sns):
 
 	try:
 		# 현재 날짜 구하기
-		saveDate = datetime.today().strftime("%Y-%m-%d %H:%M:%S") # YYYY/mm/dd HH:MM:SS 형태의 시간 출력
+		updated_date = datetime.today().strftime("%Y-%m-%d %H:%M:%S") # YYYY/mm/dd HH:MM:SS 형태의 시간 출력
 		# ip 주소 확인
-		saveIp = requests.get("http://ip.jsontest.com").json()['ip']
+		updated_ip = requests.get("http://ip.jsontest.com").json()['ip']
 
 		cursor = conn.cursor()
 		# insert 처리
-		sql = """INSERT INTO member 
-		(mail, name, telno, addr, sns, date_created, ip) 
-		VALUES(%s, %s, %s, %s, %s, %s, %s)"""
-		val = [mail, name, telno, addr, sns, saveDate, saveIp]
+		sql = """
+		update member set 
+		mail = %s,
+		name = %s,
+		telno = %s,
+		addr = %s,
+		sns = %s,
+		last_updated_date = %s,
+		last_updated_ip = %s
+		WHERE id = %s """
+		val = [mail, name, telno, addr, sns, updated_date, updated_ip, idx]
 		
 		cursor.execute(sql, val)
 		conn.commit() # DB에 데이터 기록
-		print("데이터를 기록하였습니다.")
+		print(sql)
+		print("데이터가 수정되었습니다.")
 		'''
         print(mails)
 		print(names)
@@ -46,7 +54,7 @@ def MemDbAdd(mail, name, telno, addr, sns):
 		'''
 
 	except:
-		print("입력 에러입니다.")
+		print("DB 입력 에러입니다.")
 
 	conn.close()
 	
